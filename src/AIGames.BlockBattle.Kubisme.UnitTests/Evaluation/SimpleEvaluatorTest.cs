40a3451f49ff868714a4f403e0de7e95886bd798
Corniel <mail@corniel.nl>
2015-07-18 16:22:14 +0200

    Fixed determenation of the attachments to the right wall, and the count of blockades. Added unit tests that fully covered the SimpleEvaluatorTest.

---
+++ src/AIGames.BlockBattle.Kubisme.UnitTests/Evaluation/SimpleEvaluatorTest.cs
@@ -0,0 +1,194 @@
+﻿using AIGames.BlockBattle.Kubisme.Evaluation;
+using AIGames.BlockBattle.Kubisme.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIGames.BlockBattle.Kubisme.UnitTests.Evaluation
+{
+	[TestFixture]
+	public class SimpleEvaluatorTest
+	{
+		[Test]
+		public void Test_Blockades_21()
+		{
+			var field = @"
+..........
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+...XXX.XXX
+XXX.......";
+			var pars = new SimpleParameters()
+			{
+				Blockades = 1,
+			};
+			var expected = 21;
+
+			Test(field, pars, expected);
+		}
+
+		[Test]
+		public void Test_Floor_3()
+		{
+			var field = @"
+..........
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+...XXX.XXX
+XXX.......";
+			var pars = new SimpleParameters()
+			{
+				Floor = 1,
+			};
+			var expected = 3;
+
+			Test(field, pars, expected);
+		}
+
+		[Test]
+		public void Test_Holes_16()
+		{
+			var field = @"
+..........
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+...XXX.XXX
+XXX.......";
+			var pars = new SimpleParameters()
+			{
+				Holes = 1,
+			};
+			var expected = 16;
+
+			Test(field, pars, expected);
+		}
+
+		[Test]
+		public void Test_NeighborsHorizontal_6()
+		{
+			var field = @"
+..........
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+...XXX.XXX
+XXX.......";
+			var pars = new SimpleParameters()
+			{
+				NeighborsHorizontal = 1,
+			};
+			var expected = 6;
+
+			Test(field, pars, expected);
+		}
+
+		[Test]
+		public void Test_NeighborsVertical_15()
+		{
+			var field = @"
+..........
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+...XXX.XXX
+XXX.......";
+			var pars = new SimpleParameters()
+			{
+				NeighborsVertical = 1,
+			};
+			var expected = 15;
+
+			Test(field, pars, expected);
+		}
+
+		[Test]
+		public void Test_WallsLeft_3()
+		{
+			var field = @"
+..........
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+X..XXX.XXX
+XXX......X";
+			var pars = new SimpleParameters()
+			{
+				WallsLeft = 1,
+			};
+			var expected = 3;
+
+			Test(field, pars, expected);
+		}
+
+		[Test]
+		public void Test_WallsLeftWithInterception_1()
+		{
+			var field = @"
+..........
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+...XXX.XXX
+XXX......X";
+			var pars = new SimpleParameters()
+			{
+				WallsLeft = 1,
+			};
+			var expected = 1;
+
+			Test(field, pars, expected);
+		}
+		
+		[Test]
+		public void TestWallsRight_4()
+		{
+			var field = @"
+..........
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+X..XXX.XXX
+XXX......X";
+			var pars = new SimpleParameters()
+			{
+				WallsRight = 1,
+			};
+			var expected = 4;
+
+			Test(field, pars, expected);
+		}
+
+		[Test]
+		public void TestWallsRightNoneOnTheFloor_0()
+		{
+			var field = @"
+.........X
+..X.......
+.X.XXXXXXX
+XXX...X.XX
+X..XXX.XXX
+XXX.......";
+			var pars = new SimpleParameters()
+			{
+				WallsRight = 1,
+			};
+			var expected = 0;
+
+			Test(field, pars, expected);
+		}
+
+		private static void Test(string str, SimpleParameters pars, int expected)
+		{
+			var field = Field.Create(0, 0, str);
+			var evaluator = new SimpleEvaluator() { Parameters = pars };
+			var actual = evaluator.GetScore(field);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
--- src/AIGames.BlockBattle.Kubisme/Evaluation/SimpleEvaluator.cs
+++ src/AIGames.BlockBattle.Kubisme/Evaluation/SimpleEvaluator.cs
@@ -42,6 +42,7 @@ namespace AIGames.BlockBattle.Kubisme.Evaluation
 
 		protected SimpleParameters pars { get; set; }
 
+		/// <summary>Gets a score only based on characters of the current state.</summary>
 		public int GetScore(Field field)
 		{
 			var score = 0;
@@ -49,18 +50,20 @@ namespace AIGames.BlockBattle.Kubisme.Evaluation
 			score += field.Combo * pars.Combo;
 
 			int filterTopColomns = 0;
-			int filterBlocades = 0;
+			int filterBlockades = 0;
 			var holes = 0;
 			var wallLeft = 0;
 			var wallRight = 0;
-			var blokades = 0;
+			var blockades = 0;
 			var neighborsH = 0;
 			var neighborsV = 0;
 			ushort previous = 0;
 
+			// loop through the rows.
 			for (var r = 0; r < field.RowCount; r++)
 			{
-				var row = field[r].row;
+				var rw = field[r];
+				var row = rw.row;
 				if (row == Row.Empty) { continue; }
 
 				var rowCount = Row.Count[row];
@@ -69,8 +72,7 @@ namespace AIGames.BlockBattle.Kubisme.Evaluation
 
 				score += pars.RowCountWeights[rowCount] * pars.RowWeights[r];
 				holes += Row.Count[holesMask];
-				blokades += Row.Count[filterBlocades & row];
-
+				
 				// Give points for blocks against the wall, that are not under an hole.
 				if ((row & MaskWallLeft) != 0)
 				{
@@ -90,17 +92,29 @@ namespace AIGames.BlockBattle.Kubisme.Evaluation
 					wallRight = 0;
 				}
 
-				filterBlocades |= holesMask;
 				filterTopColomns |= row;
 
 				neighborsV += NeighborsVertical[row];
 				neighborsH += Row.Count[row & previous];
 				previous = row;
 			}
+
+			// loop for blockades too.
+			for (var r = field.RowCount - 1; r >= 0; r--)
+			{
+				var rw = field[r];
+				var row = rw.row;
+				if (row == Row.Empty) { break; }
+				filterBlockades |= Row.Filled ^ row;
+
+				var blockadesMask = filterBlockades & row;
+				blockades += Row.Count[blockadesMask];
+			}
+
 			score += wallLeft * pars.WallsLeft;
-			score += wallRight + pars.WallsRight;
+			score += wallRight * pars.WallsRight;
 			score += holes * pars.Holes;
-			score += blokades * pars.Blockades;
+			score += blockades * pars.Blockades;
 			score += neighborsH * pars.NeighborsHorizontal;
 			score += neighborsV * pars.NeighborsVertical;
 			score += Row.Count[previous] * pars.Floor;

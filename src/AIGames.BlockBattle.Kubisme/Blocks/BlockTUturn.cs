d8e2d66a47abbf8105bd393f46e29d3ba1da9b91
Corniel <mail@corniel.nl>
2015-09-14 17:20:25 +0200

    Tweak for GetPath() for T u-turn block.

--- src/AIGames.BlockBattle.Kubisme/Blocks/BlockTUturn.cs
+++ src/AIGames.BlockBattle.Kubisme/Blocks/BlockTUturn.cs
@@ -1,8 +1,5 @@
-﻿using System;
+﻿using AIGames.BlockBattle.Kubisme.Communication;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AIGames.BlockBattle.Kubisme
 {
@@ -74,13 +71,51 @@ namespace AIGames.BlockBattle.Kubisme
 
 		public override int Width { get { return 3; } }
 
-		public override IEnumerable<int> GetColumns(Field field) { return GetColumnsJLTUTurn(field); }
-		public override BlockPath GetPath(Field field, int column) { return pathsJLTUturn[column]; }
-
 		public override Block TurnLeft() { return this[RotationType.Right]; }
 		public override Block TurnRight() { return this[RotationType.Left]; }
 
 		public override Position TurnLeft(Position position) { return new Position(position.Col + 1, position.Row - 1); }
 		public override Position TurnRight(Position position) { return new Position(position.Col, position.Row - 1); }
+
+		public override IEnumerable<int> GetColumns(Field field)
+		{
+			if (field.FirstFilled > 1)
+			{
+				foreach (var col in Columns)
+				{
+					yield return col;
+				}
+			}
+			else
+			{
+				var row = field[0];
+				foreach (var column in Columns)
+				{
+					if ((accessibles[column] & row) == 0)
+					{
+						yield return column;
+					}
+				}
+			}
+		}
+
+		public override BlockPath GetPath(Field field, int column)
+		{
+			return pathsTUTurn[column];
+		}
+
+		public static readonly BlockPath[] pathsTUTurn = new BlockPath[]
+		{
+			BlockPath.Create(ActionType.Left, ActionType.Left, ActionType.Left, ActionType.TurnLeft, ActionType.TurnLeft, ActionType.Drop),
+			BlockPath.Create(ActionType.Left, ActionType.Left, ActionType.TurnLeft, ActionType.TurnLeft, ActionType.Drop),
+			BlockPath.Create(ActionType.Left, ActionType.TurnLeft, ActionType.TurnLeft, ActionType.Drop),
+			
+			BlockPath.Create(ActionType.TurnLeft, ActionType.TurnLeft, ActionType.Drop),
+
+			BlockPath.Create(ActionType.Right, ActionType.TurnRight, ActionType.TurnRight, ActionType.Drop),
+			BlockPath.Create(ActionType.Right, ActionType.Right, ActionType.TurnRight, ActionType.TurnRight, ActionType.Drop),
+			BlockPath.Create(ActionType.Right, ActionType.Right, ActionType.Right, ActionType.TurnRight, ActionType.TurnRight, ActionType.Drop),
+			BlockPath.Create(ActionType.Right, ActionType.Right, ActionType.Right, ActionType.Right, ActionType.TurnRight, ActionType.TurnRight, ActionType.Drop),
+		};
 	}
 }

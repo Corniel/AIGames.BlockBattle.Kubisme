b364f34c1b59035b107e91cbd12a5841656f3afa
Corniel <mail@corniel.nl>
2015-11-06 07:25:10 +0100

    Garbage was not a good idea.

--- src/AIGames.BlockBattle.Kubisme/DecisionMaking/BlockNode.cs
+++ src/AIGames.BlockBattle.Kubisme/DecisionMaking/BlockNode.cs
@@ -7,13 +7,7 @@ namespace AIGames.BlockBattle.Kubisme
 		public static Field Apply(Field field, int depth, ApplyParameters pars)
 		{
 			// Lock a row every 15 turns. 
-			// Alternatively Just to create some garbage. We use a lock because adding
-			// a garbage line adds to much randomness to the outcome.
-			if (depth == 1)
-			{
-				return field.SimulateGarbage(pars.FirstFilled);
-			}
-			else if ((pars.Round + depth) % 15 == 0 || (depth & 1) == 1)
+			if ((pars.Round + depth) % 15 == 0)
 			{
 				return field.LockRow();
 			}

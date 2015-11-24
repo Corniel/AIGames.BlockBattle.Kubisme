7638f8ce0475ac93e9334410c7da62fc267a5d7e
Corniel <mail@corniel.nl>
2015-11-24 12:04:12 +0100

    Added a test to output the current bot occording to the Evualtion parameters.

---
+++ src/AIGames.BlockBattle.Kubisme.UnitTests/Genetics/SimulationBotCollectionTest.cs
@@ -0,0 +1,29 @@
+﻿using AIGames.BlockBattle.Kubisme.Genetics;
+using AIGames.BlockBattle.Kubisme.Genetics.Serialization;
+using NUnit.Framework;
+using System.IO;
+
+namespace AIGames.BlockBattle.Kubisme.UnitTests.Genetics
+{
+	[TestFixture]
+	public class SimulationBotCollectionTest
+	{
+		[Test]
+		public void Save_OneBot_ToFile()
+		{
+			var collection = new SimulationBotCollection()
+			{
+				new BotData()
+				{
+					Id = 17,
+					Locked = true,
+					DefPars = EvaluatorParameters.GetDefault(),
+					EndPars = EvaluatorParameters.GetEndGame(),
+				},
+			};
+
+			var file = new FileInfo("collection.xml");
+			collection.Save(file);
+		}
+	}
+}
